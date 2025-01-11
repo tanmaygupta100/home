afbd63f8704fbfcb67b4c39c18b8431965c00cb7
Tanmay <112268448+tanmaygupta100@users.noreply.github.com>
2025-01-11 02:08:19 -0800

    HOME-1: Properly aligned footer
    Aligned footer to the absolute bottom of every page.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer>
+        <footer className="w-full absolute bottom-0 text-center p-4 bg-gray-200">
             <p>&copy; 2025 Tanmay Gupta</p>
         </footer>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,7 +31,6 @@ const Header = () => {
                 return [];
         }
     };
-
     const buttons = getButtons();
 
 
