5f969053bb1198342446e37ad9bc493628ceca1e
Tanmay <112268448+tanmaygupta100@users.noreply.github.com>
2025-01-10 22:10:27 -0800

    HOME-1: Set up routes and base files
    Set up routes, component files, and pages files.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Skills from './pages/Skills';
+import Projects from './pages/Projects';
+import { Route, Router, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </Router>
+    <>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+        <Footer />
+    </>
   );
 }
 
