a9b624789c43c9f4ae2931c7e2df36349ae75001
Tanmay <112268448+tanmaygupta100@users.noreply.github.com>
2025-01-10 19:39:35 -0800

    Initial React app setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
