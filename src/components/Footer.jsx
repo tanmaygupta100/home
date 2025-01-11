5683efeabfd6684911275311e447b973fbc44528
Tanmay <112268448+tanmaygupta100@users.noreply.github.com>
2025-01-11 02:41:09 -0800

    HOME-2: Added grid design at footer.
    Added vaporwave-style grid design at the footer, and rules in index.css to keep the footer at the base.
    
    Still need to do more testing with the mobile-view to ensure users can't scroll past footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,36 @@
 const Footer = () => {
     return (
-        <footer className="w-full absolute bottom-0 text-center p-4 bg-gray-200">
-            <p>&copy; 2025 Tanmay Gupta</p>
-        </footer>
+        <div className="main-content">
+            <footer className="w-full absolute bottom-0 flex flex-col items-center">
+                <div className="flex gap-44 mt-2 relative">
+                    <div className="w-full h-[40px] bg-white absolute top-0 z-10"></div>
+                    {/* Angled Lines */}
+                    {Array.from({ length: 11 }).map((_, index) => {
+                        const angle = (index - 5) * -10;
+                        return (
+                            <div
+                                key={`angled-${index}`}
+                                className="w-1 h-40 bg-black"
+                                style={{
+                                    transform: `rotate(${angle}deg)`,
+                                    transformOrigin: "bottom center",
+                                }}
+                            ></div>
+                        );
+                    })}
+                    
+                    {/* Horizontal Lines */}
+                    {Array.from({ length: 4 }).map((_, index) => (
+                        <div
+                            key={`horizontal-${index}`}
+                            className="w-full h-[2px] bg-black absolute"
+                            style={{ bottom: `${index * 36}px` }}
+                        ></div>
+                    ))}
+                </div>
+            </footer>
+        </div>
+
     );
 };
 
